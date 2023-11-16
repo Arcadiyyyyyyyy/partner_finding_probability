42080e44f827884b34a0330c3aa05314f43b0a68
Anton <109826223+Arcadiyyyyyyyy@users.noreply.github.com>
2023-11-16 21:14:09 +0000

    feat: change return string to test cd

--- backend/api/JavaApi/src/main/java/org/acme/ExampleResource.java
+++ backend/api/JavaApi/src/main/java/org/acme/ExampleResource.java
@@ -11,6 +11,6 @@ public class ExampleResource {
     @GET
     @Produces(MediaType.TEXT_PLAIN)
     public String hello() {
-        return "Hello from " + System.getenv().getOrDefault("HOSTNAME", "unknown");
+        return "'Hola, get up' from " + System.getenv().getOrDefault("HOSTNAME", "unknown");
     }
 }
